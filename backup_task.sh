9f96b88fb0fcb6589bec5e49f72ba647ca771e49
lihonglin05 <lihonglin05@meituan.com>
2020-11-15 11:32:07 +0800

    good

--- backup_task.sh
+++ backup_task.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+starttime=$(date +%Y-%m-%d\ %H:%M:%S)
+echo $starttime
+echo "===============开始备份任务==============\n"
+
 git add .
 git commit -m "good"
 git push 
+
+endtime=$(date +%Y-%m-%d\ %H:%M:%S)
+echo $endtime
+echo "==============备份完成===================="
