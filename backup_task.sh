f32ceb4e0605a230aed55efd881e55e7ce7555f0
lihonglin05 <lihonglin05@meituan.com>
2020-11-15 12:05:36 +0800

    good

--- backup_task.sh
+++ backup_task.sh
@@ -3,9 +3,6 @@
 dir=`dirname "$0"`
 cd $dir
 
-tail -n 10 backup.log > backup.log
-tail -n 10 backup.err > backup.err
-
 
 starttime=$(date +%Y-%m-%d\ %H:%M:%S)
 echo "=======$starttime========开始备份任务=============="
@@ -16,3 +13,7 @@ git push
 
 endtime=$(date +%Y-%m-%d\ %H:%M:%S)
 echo "========$endtime======备份完成===================="
+
+
+tail -n 10 backup.log > backup.log
+tail -n 10 backup.err > backup.err
